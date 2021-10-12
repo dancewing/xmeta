7d5de61f3e2af8f06880e0329bfa75653a4f1a4d
jeff_qian <baojian_qian@wishbuild.com>
2021-10-13 05:54:04 +0800

    change to yarn

--- xmeta-server-admin-ui/packages/amplication-client/src/Entity/SchemaField.tsx
+++ xmeta-server-admin-ui/packages/amplication-client/src/Entity/SchemaField.tsx
@@ -105,6 +105,15 @@ export const SchemaField = ({
             />
           );
         }
+        case "#/definitions/RelationType": {
+          return (
+              <RelationAllowMultipleField
+                  fieldName={fieldName}
+                  isDisabled={isDisabled}
+                  entityDisplayName={entityDisplayName}
+              />
+          );
+        }
       }
 
       throw new Error(`Unexpected propertySchema.type: ${propertySchema.type}`);
